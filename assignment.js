ceff54402fb707841b643176a5fe348039012b4a
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-25 21:39:54 +0600

    hotelCost added

--- assignment.js
+++ assignment.js
@@ -20,8 +20,33 @@ function budgetCalculator(num1, num2, num3) {
     laptopTotalPrize = 500 * num3;
 
     totalBudget = goriTotalPrize + phoneTotalPrize + laptopTotalPrize;
-    
+
     return totalBudget;
 }
 budgetCalculator(10, 10, 10);
+
+//hotelCost
+
+function hotelCost(days) {
+    var hotelRent = 0;
+    if (days <= 10) {
+        hotelRent = days * 100;
+    }
+    else if (days <= 20) {
+        var firstTenDays = 10 * 100;
+        var remaining = days - 10;
+        var secondTenDays = remaining * 80;
+        hotelRent = firstTenDays + secondTenDays;
+    }
+    else {
+        var firstTenDays = 10 * 100;
+        var secondTenDays = 10 * 80;
+        var remaining = days - 20;
+        var twentyPlusDays = remaining * 50;
+
+        hotelRent = firstTenDays + secondTenDays + twentyPlusDays;
+    }
+    return hotelRent;
+}
+hotelCost(25);
 
