9c148e5a22b52001d9b08ff11dfa595e7981ae5a
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-25 21:16:58 +0600

    budgetCalculator added

--- assignment.js
+++ assignment.js
@@ -1,3 +1,4 @@
+// https://github.com/iamjahid697/assignment03/blob/main/assignment.js
 
 //kilometerToMeter
 
@@ -7,5 +8,20 @@ function kilometerToMeter(kilometer){
 }
 kilometerToMeter(10);
 
+//budgetCalculator
 
+function budgetCalculator(num1, num2, num3) {
+    goriPrize = 50;
+    phonePrize = 100;
+    laptopPrize = 500;
+
+    goriTotalPrize = 50 * num1;
+    phoneTotalPrize = 100 * num2;
+    laptopTotalPrize = 500 * num3;
+
+    totalBudget = goriTotalPrize + phoneTotalPrize + laptopTotalPrize;
+    
+    return totalBudget;
+}
+budgetCalculator(10, 10, 10);
 
