5d2945d57ee179f21fea9d3fb5d55eeccdc83057
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-25 23:56:23 +0600

    added input condition for bonus marks

--- assignment.js
+++ assignment.js
@@ -1,14 +1,20 @@
 // https://github.com/iamjahid697/assignment03/blob/main/assignment.js
 
-//kilometerToMeter
+     //kilometerToMeter
 
-function kilometerToMeter(kilometer){
-    meter = kilometer * 1000;
-    return meter;
+function kilometerToMeter(kilometer) {
+    if (kilometer <= 0 || typeof kilometer == "string" || typeof kilometer == "boolean" || typeof kilometer == "object") {
+        console.log("Invalid value")
+    }
+    else {
+        meter = kilometer * 1000;
+        return meter;
+    }
 }
 kilometerToMeter(10);
 
-//budgetCalculator
+
+     //budgetCalculator
 
 function budgetCalculator(num1, num2, num3) {
     goriPrize = 50;
@@ -25,11 +31,14 @@ function budgetCalculator(num1, num2, num3) {
 }
 budgetCalculator(10, 10, 10);
 
-//hotelCost
+      //hotelCost
 
 function hotelCost(days) {
     var hotelRent = 0;
-    if (days <= 10) {
+    if (days <= 0 || typeof days == 'string' || typeof days == 'boolean' || typeof days == 'object') {
+        console.log("Enter valid days");
+    }
+    else if (days <= 10) {
         hotelRent = days * 100;
     }
     else if (days <= 20) {
@@ -50,11 +59,15 @@ function hotelCost(days) {
 }
 hotelCost(25);
 
-// megaFriend
+          // megaFriend
 
 function megaFriend(arr) {
     var longestWord = "";
-
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== "string") {
+            console.log("Give only string values");
+        }
+    }
     for (var i = 0; i < arr.length; i++) {
         if (longestWord.length < arr[i].length) {
             longestWord = arr[i];
