297bda01cc05c7a81bf3e0e399b036fe80622be1
Md Jahidul Islam <iamjahid697@gmail.com>
2021-01-25 20:53:51 +0600

    added kilometerToMeter comment and remove console.log

--- assignment.js
+++ assignment.js
@@ -1,6 +1,11 @@
+
+//kilometerToMeter
+
 function kilometerToMeter(kilometer){
     meter = kilometer * 1000;
     return meter;
 }
-var result = kilometerToMeter(10);
-console.log(result);
+kilometerToMeter(10);
+
+
+
